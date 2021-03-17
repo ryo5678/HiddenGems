a375ffe3f2b0fc8f5ae1669b7f4fc5879946d789
Swapnil Katakwar <skatakwa@uncc.edu>
2021-03-16 20:12:12 -0400

    flask app updated welcome message with HTML

--- 3155Activities/flask_app/flask01.py
+++ 3155Activities/flask_app/flask01.py
@@ -11,7 +11,7 @@ app = Flask(__name__)     # create an app
 # get called. What it returns is what is shown as the web page
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Welcome, Notes App User!'
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
