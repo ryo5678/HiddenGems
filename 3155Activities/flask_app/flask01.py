e15e5edcb0ff861363d5b4666c6660830057626e
Swapnil Katakwar <skatakwa@uncc.edu>
2021-03-18 23:10:01 -0400

    flask app #2 - created a dynamic welcom route

--- 3155Activities/flask_app/flask01.py
+++ 3155Activities/flask_app/flask01.py
@@ -12,10 +12,12 @@ app = Flask(__name__)     # create an app
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
+@app.route('/')
 @app.route('/index')
 def index():
-   
-     return render_template('index.html')
+ a_user = {'name': 'Mogli', 'email':'mogli@uncc.edu'}
+
+ return render_template('index.html', user = a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
