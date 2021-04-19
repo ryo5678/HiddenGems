b2fdae38efbb677732075c4617fc5996d2949547
skatakwa <skatakwa@uncc.edu>
2021-04-19 18:57:11 -0400

    flask app #3 - saving note to mock list of notes

--- 3155Activities/NewSave/flask_app/flask03.py
+++ 3155Activities/NewSave/flask_app/flask03.py
@@ -2,16 +2,19 @@ import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
+from flask import redirect, url_for
 
 
 app = Flask(__name__)     # create an app
 
+notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+         2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+         3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
+         }
+
 @app.route('/notes')
 def get_notes():
-    notes = {1:{'title':'First note','text':'This is my first note','date':'10-1-2020'},
-             2:{'title':'Second note','text':'This is my second note','date':'10-2-2020'},
-             3:{'title':'Third note','text':'This is my third note','date':'10-3-2020'}
-             }
+
     a_user = {'name': 'Swapnil Katakwar', 'email': 'mogli@uncc.edu'}
     return render_template('notes.html', user=a_user, notes=notes)
 
@@ -20,17 +23,22 @@ def new_note():
     a_user = {'name': 'Swapnil Katakwar', 'email': 'mogli@uncc.edu'}
     print('request method is',request.method)
     if request.method == 'POST':
-        request_data = request.form
-        return f"data: {request_data}  !"
+        title = request.form['title']
+        text = request.form['noteText']
+        from datetime import date
+        today = date.today()
+        today = today.strftime("%m-%d-%Y")
+        id = len(notes)+1
+        notes[id] = {'title': title, 'text': text, 'date': today}
+
+        return redirect(url_for('get_notes', name =a_user))
     else:
         return render_template('new.html', user=a_user)
 
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    notes = {1:{'title':'First note','text':'This is my first note','date':'10-1-2020'},
-             2:{'title':'Second note','text':'This is my second note','date':'10-2-2020'}
-             }
+
     a_user = {'name': 'Swapnil Katakwar', 'email': 'mogli@uncc.edu'}
     return render_template('note.html',note=notes[int(note_id)], user=a_user)
 @app.route('/')
