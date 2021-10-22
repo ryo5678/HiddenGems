ab7d8a8fa17cd8fb2b221f284d6cf9a6d8c14752
Kyle Vergnetti <kvergnet@uncc.edu>
2021-10-22 12:33:57 -0400

    Added data models

--- app/src/main/java/com/example/hiddengems/dataModels/location.java
+++ app/src/main/java/com/example/hiddengems/dataModels/location.java
@@ -1,5 +1,26 @@
 package com.example.hiddengems.dataModels;
 
+import android.media.Image;
+
 public class location {
+    String Name;
+    String Address;
+    String Category;
+    String[] Tags;
+    //operatinghours[] Hours;
+    int startTime;
+    int endTime;
+    menuItem[] Menu;
+    Image[] Images;
+    boolean isEvent;
+    int startDate;
+    int endDate;
+    String Season;
+    int Likes;
+    String Rating;
+    String[] Comments;
+    String[] Reviews;
 
 }
+
+
