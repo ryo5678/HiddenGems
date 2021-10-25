1a68d7cb2a7a9b0e3eece64bef55a8453576f702
Ritik Parekh <rparekh2@uncc.edu>
2021-10-24 22:11:11 -0400

    stuff for search page

--- app/src/main/java/com/example/hiddengems/search/SearchScreenFragment.java
+++ app/src/main/java/com/example/hiddengems/search/SearchScreenFragment.java
@@ -1,10 +1,12 @@
 package com.example.hiddengems.search;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,14 +15,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.hiddengems.R;
+import com.example.hiddengems.databinding.FragmentRequestTagsBinding;
+import com.example.hiddengems.databinding.FragmentSearchScreenBinding;
 
 
 public class SearchScreenFragment extends Fragment {
 
     String SelectedFilter;
-
+    FragmentSearchScreenBinding binding;
+    String text;
 
     public SearchScreenFragment() {
         // Required empty public constructor
@@ -51,6 +57,20 @@ public class SearchScreenFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Search");
+        binding.searchSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                text = binding.enterLocation.getText().toString();
+                if (text.isEmpty()) {
+                    missingInput(getActivity());
+                } else {
+                    searchLocation(text);
+                }
+            }
+        });
+
+
     }
 
     @Override
@@ -65,5 +85,42 @@ public class SearchScreenFragment extends Fragment {
         //getActivity().setTitle(SelectedFilter);
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void searchLocation(String text){
+        //String token = mUserToken.token;
+        /*FormBody formBody = new FormBody.Builder()
+                .add("post_text", text)
+                .build();
+        Request request = new Request.Builder()
+                .url("https://www.theappsdr.com/posts/create")
+                .addHeader("Authorization", "BEARER "  + token)
+                .post(formBody)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+                if(response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    String body = responseBody.string();
+                    Log.d(TAG,body);
+                    */
+        FragmentManager fm = getActivity()
+                .getSupportFragmentManager();
+        //fm.popBackStack("Search", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                /*}
+            }
+        });*/
+    }
+    public void missingInput(Context context){
+        Toast.makeText(context, getString(R.string.missing),Toast.LENGTH_SHORT).show();
+    }
+
 
 }
