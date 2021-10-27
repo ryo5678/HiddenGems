9bf9fc206acbe2c1c37a15418a5664325ea88fb7
ryo5678 <ryo5678craft@gmail.com>
2021-10-26 21:01:32 -0400

    Navigation bar

--- app/src/main/java/com/example/hiddengems/MainActivity.java
+++ app/src/main/java/com/example/hiddengems/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hiddengems;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,16 +12,23 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.hiddengems.databinding.ActivityMainBinding;
+import com.example.hiddengems.map.MapsActivity;
 import com.example.hiddengems.profile.*;
 import com.example.hiddengems.home.*;
+import com.example.hiddengems.search.SearchScreenFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements ProfileFragment.profile {
+public class MainActivity extends AppCompatActivity implements ProfileFragment.profile, BottomNavigationView.OnNavigationItemSelectedListener {
 
     ActivityMainBinding binding;
+    HomeFragment homeFragment = new HomeFragment();
+    SearchScreenFragment searchFragment = new SearchScreenFragment();
+    AddScreenFragment addFragment = new AddScreenFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +37,41 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.p
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_add, R.id.navigation_map, R.id.navigation_profile)
-                .build();
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
-        NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        navView.setOnNavigationItemSelectedListener(this);
+        navView.setSelectedItemId(R.id.navigation_home);
+
 
         /*getSupportFragmentManager().beginTransaction()
                 .add(R.id.rootView, new HomeFragment())
                 .commit();*/
 
     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, homeFragment).commit();
+                return true;
+            case R.id.navigation_search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, searchFragment).commit();
+                return true;
+            case R.id.navigation_add:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, addFragment).commit();
+                return true;
+            case R.id.navigation_map:
+
+            case R.id.navigation_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, profileFragment).commit();
+                return true;
+        }
+        return false;
+    }
 
     @Override
     public void tagRequest() {
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.rootView, RequestTagsFragment.newInstance())
+                .replace(R.id.fragmentContainerView, RequestTagsFragment.newInstance())
                 .addToBackStack("TagRequest")
                 .commit();
     }
@@ -56,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.p
     @Override
     public void operatingHoursRequest() {
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.rootView, HoursChangeFragment.newInstance())
+                .replace(R.id.fragmentContainerView, HoursChangeFragment.newInstance())
                 .addToBackStack("operatingHoursRequest")
                 .commit();
     }
@@ -64,7 +87,7 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.p
     @Override
     public void contactUs() {
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.rootView, ContactUsFragment.newInstance())
+                .replace(R.id.fragmentContainerView, ContactUsFragment.newInstance())
                 .addToBackStack("contactUs")
                 .commit();
     }
