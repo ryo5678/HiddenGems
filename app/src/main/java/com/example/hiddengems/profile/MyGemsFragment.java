33b9b9800df4096397deccc790e94e359f95524f
skatakwa <65989376+skatakwa@users.noreply.github.com>
2021-10-28 17:06:29 -0400

    Update MyGemsFragment.java

--- app/src/main/java/com/example/hiddengems/profile/MyGemsFragment.java
+++ app/src/main/java/com/example/hiddengems/profile/MyGemsFragment.java
@@ -1,66 +1,107 @@
+
+
 package com.example.hiddengems.profile;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.hiddengems.R;
+import com.example.hiddengems.databinding.FragmentHoursChangeBinding;
+import com.example.hiddengems.databinding.FragmentLocationRemovalBinding;
+import com.example.hiddengems.databinding.FragmentMyGemsBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyGemsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MyGemsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentMyGemsBinding binding;
+    String text;
 
     public MyGemsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyGemsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyGemsFragment newInstance(String param1, String param2) {
+
+    public static MyGemsFragment newInstance() {
         MyGemsFragment fragment = new MyGemsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_gems, container, false);
+        binding = FragmentMyGemsBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("My Gems");
+
+        binding.myGems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text = binding.gemss.getText().toString();
+                if (text.isEmpty()) {
+                    missingInput(getActivity());
+                } else {
+                    submitTags(text);
+                }
+            }
+        });
+
+
+    }
+
+    public void submitTags(String text) {
+        //String token = mUserToken.token;
+        /*FormBody formBody = new FormBody.Builder()
+                .add("post_text", text)
+                .build();
+        Request request = new Request.Builder()
+                .url("https://www.theappsdr.com/posts/create")
+                .addHeader("Authorization", "BEARER "  + token)
+                .post(formBody)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+                if(response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    String body = responseBody.string();
+                    Log.d(TAG,body);
+                    */
+        FragmentManager fm = getActivity()
+                .getSupportFragmentManager();
+        fm.popBackStack("gems", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                /*}
+            }
+        });*/
+    }
+
+    public void missingInput(Context context) {
+        Toast.makeText(context, getString(R.string.missing), Toast.LENGTH_SHORT).show();
     }
 }
