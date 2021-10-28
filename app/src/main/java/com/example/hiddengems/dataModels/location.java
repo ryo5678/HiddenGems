1418dfff28fd003f5cb5261589faa9b07bffe609
Kyle Vergnetti <kvergnet@uncc.edu>
2021-10-28 17:34:54 -0400

    location changes

--- app/src/main/java/com/example/hiddengems/dataModels/location.java
+++ app/src/main/java/com/example/hiddengems/dataModels/location.java
@@ -5,16 +5,17 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 
+
 public class location implements Serializable {
     public String Name;
     public String Address;
     public String Category;
-    public String[] Tags;
+    public ArrayList<String> Tags;
     //operatinghours[] Hours;
     public int startTime;
     public int endTime;
-    public menuItem[] Menu;
-    public Image[] Images;
+    public ArrayList<menuItem> Menu;
+    public ArrayList<Image> Images;
     public boolean isEvent;
     public int startDate;
     public int endDate;
@@ -22,15 +23,83 @@ public class location implements Serializable {
     public int Likes;
     public int currentRating;
     public int numberofRatings;
-    public String[] Comments;
-    public String[] Reviews;
+    public ArrayList<String> Comments;
+    public ArrayList<String> Reviews;
 
 
-    public location(String Name, String Address, String Category, String[] Tags) {
+    public location(String Name, String Address, String Category) {
         this.Name = Name;
         this.Address = Address;
         this.Category = Category;
-        this.Tags = Tags;
+        startTime = 0;
+        endTime = 0;
+        isEvent = false;
+        startDate = 0;
+        endDate = 0;
+        Season = "";
+        Likes = 0;
+        currentRating = 0;
+        numberofRatings = 0;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public void setAddress(String Address){
+        this.Address = Address;
+    }
+    public void setCategory(String Category){
+        this.Category= Category;
+    }
+    public void addTag(String Tag){
+        this.Tags.add(Tag);
+    }
+    public void setStartTime(int startTime){
+        this.startTime = startTime;
+    }
+    public void setEndTime(int endTime){
+        this.endTime = endTime;
+    }
+    public void addImage(Image pic){
+        this.Images.add(pic);
+    }
+    public void addMenuItem(menuItem Item){
+        this.Menu.add(Item);
+    }
+    public void setIsEvent(boolean isEvent){
+        this.isEvent = isEvent;
+    }
+    public void setStartDate(int startDate){
+        this.startDate = startDate;
+    }
+    public void setEndDate(int endDate){
+        this.endDate = endDate;
+    }
+    public void setSeason(String Season){
+        this.Season = Season;
+    }
+    public int getLikes(){
+        return this.Likes;
+    }
+    public void setLikes(int Likes){
+        this.Likes = Likes;
+    }
+    public int getCurrentRating(){
+        return this.currentRating;
+    }
+    public void setCurrentRating(int currentRating){
+        this.currentRating = currentRating;
+    }
+    public int getNumberofRatings(){
+        return this.numberofRatings;
+    }
+    public void setNumberofRatings(int numberofRatings){
+        this.numberofRatings = numberofRatings;
+    }
+    public void addComment(String comment){
+        this.Comments.add(comment);
+    }
+    public void addReview(String review){
+        this.Reviews.add(review);
     }
 
 
