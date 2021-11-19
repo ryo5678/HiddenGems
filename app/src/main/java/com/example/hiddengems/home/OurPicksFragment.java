27db79121d02d72f7e8ce52fb4429057d9585633
ryo5678 <ryo5678craft@gmail.com>
2021-11-18 21:43:29 -0500

    Ourpicks fragment

--- app/src/main/java/com/example/hiddengems/home/OurPicksFragment.java
+++ app/src/main/java/com/example/hiddengems/home/OurPicksFragment.java
@@ -2,49 +2,44 @@ package com.example.hiddengems.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hiddengems.R;
+import com.example.hiddengems.dataModels.Locations;
+import com.example.hiddengems.databinding.FragmentMyGemsBinding;
+import com.example.hiddengems.databinding.FragmentOurPicksBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OurPicksFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class OurPicksFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentOurPicksBinding binding;
+    FirebaseAuth mAuth;
+    ArrayList<Locations.Location> allLocations = new ArrayList<>();
+    ArrayList<Locations.Location> finalLocations = new ArrayList<>();
 
     public OurPicksFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OurPicksFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OurPicksFragment newInstance(String param1, String param2) {
+
+    public static OurPicksFragment newInstance() {
         OurPicksFragment fragment = new OurPicksFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -52,15 +47,44 @@ public class OurPicksFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_our_picks, container, false);
+        binding = FragmentOurPicksBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("locations")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        allLocations.clear();
+                        for(QueryDocumentSnapshot document : value) {
+
+
+                            Locations.Location newPlace = new Locations.Location(document.getId(), document.getString("Name")
+                                    ,document.getString("Category"));
+                            Log.d("Check","Adding Location");
+                            allLocations.add(newPlace);
+                        }
+                    }
+                });
+
+        mAuth = FirebaseAuth.getInstance();
+        for (int i = 0; i < allLocations.size(); i++) {
+            String match = allLocations.get(i).Name;
+            if (match == "Heist Brewery" || match == "Haberdish") {
+                finalLocations.add(allLocations.get(i));
+            }
+        }
+
     }
 }
